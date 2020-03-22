1440ae2c4f956b496a335cb3d34740810ae975cc
Dania Aflak <dania.aflak@skema.edu>
2020-03-22 14:36:54 +0100

    modif

--- src/service.js
+++
@@ -1,7 +0,0 @@
-console.log("service.js");
-const cardPlane = document.querySelector(".card-service.transferts-aeroport");
-console.log(cardPlane);
-
-console.log(document.querySelectorAll("h3"));
-const coucou = "coucoucocu";
-console.log(coucou);
